6ef1579e80ccc94cc338b20b3df3edf7efa7778c
david griffin <65681542+davidleegriffin@users.noreply.github.com>
2021-05-14 12:28:49 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-import { ApolloProvider } from "@apollo/react-hooks"
+import { ApolloProvider } from "@apollo/react-hooks";
 import { client } from "./utils/client"
 import HomePage from "./pages/HomePage"
 

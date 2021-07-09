22a82d071483324869e3e5d7bf037219627e8169
davidleegriffin <davidleegriffin@yahoo.com>
2021-07-09 16:13:44 -0500

    styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,12 +5,12 @@ import './HomePage.css';
 export default function HomePage() {
   return (
     <>
+      <HotJobs />
       <div className="navbar__container--main">
         <h1 className="navbar__header--banner">Hot Jobs and Partnerships</h1>
         <img src="https://cdn.shortpixel.ai/client/q_lossless,ret_img,w_898/https://www.growthmarketingpro.com/wp-content/uploads/2018/06/app-academy.png" alt="App Academy" width="200"></img>
       </div>
       {/* <Partnerships /> */}
-      <HotJobs />
     </>
   )
 }

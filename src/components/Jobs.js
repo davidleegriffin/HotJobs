288ec7bbc705913da750c473bc8e9abcc6b75d7f
davidleegriffin <davidleegriffin@yahoo.com>
2021-07-09 15:56:27 -0500

    regex on name

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -6,10 +6,12 @@ function Jobs(props) {
     let jobTitle = props.props.jobTitle__B;
     let locale = props.props.jobLocation__C;
     let jobLink = props.props.jobUrl__D;
+    let company = emp.replace(/(\d+)(\S?)(\s)(\d?)(reviews)/, "");
+    // console.log('company', company);
     return (
         <div className="jobs__container--main">
             <div className="jobs__container--card">
-                <div className="jobs__container--header">{emp}</div>
+                <div className="jobs__container--header">{company}</div>
                 <div className="jobs__container--title">{jobTitle}</div>
                 <div className="jobs__container--locale">{locale}</div>
                 <div className="jobs__container--link">
